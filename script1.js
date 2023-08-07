a862273c9916ba918d3fb4b798dfaf5e747900af
SASZASERGIJEWIC <129986186+SASZASERGIJEWIC@users.noreply.github.com>
2023-08-07 16:28:53 +0200

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,46 @@
+
+
+// let paragraphsArray = [
+//     "1",
+//     "1.1",
+//     "1.2",
+//     ];
+    
+//     displayParagraphs(paragraphsArray); 
+  
+// function displayParagraphs(paragraphs) {
+//     let block = document.getElementById("paragraphs");
+  
+//     block.style.color = 'green'
+
+//     paragraphs.forEach(function(Text) {
+
+//     let paragraphs1 = document.createElement("p");
+
+//     paragraphs1.className = "paragraph";
+
+//     paragraphs1.textContent = Text; 
+
+//     block.appendChild(paragraphs1);
+  
+//     paragraphs1.addEventListener("mouseover", function () {
+//         toggle(paragraphs1);
+//         });
+//     });
+//     }
+
+//     function toggle(paragraphs1) {
+
+//         if (paragraphs1.textContent.includes("*")) {
+
+//         paragraphs1.textContent = paragraphs1.originalText;
+
+//         } else {
+
+//         paragraphs1.originalText = paragraphs1.textContent;
+
+//         paragraphs1.textContent = "*".repeat(paragraphs1.textContent.length);
+        
+//         }
+//     }
+
